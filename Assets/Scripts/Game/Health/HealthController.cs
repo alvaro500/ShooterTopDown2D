f5b21b467d9380307f61db7ec17614ae60ae2106
Alvaro <alvaro500@outlook.com>
2023-05-31 16:24:50 -0500

    How to Create a Health Bar Part 2

--- Assets/Scripts/Game/Health/HealthController.cs
+++ Assets/Scripts/Game/Health/HealthController.cs
@@ -22,6 +22,7 @@ public class HealthController : MonoBehaviour
 
     public UnityEvent OnDied;
     public UnityEvent OnDamaged;
+    public UnityEvent OnHealthChanged;
 
     public void TakeDamage(float damageAmount)
     {
@@ -36,6 +37,7 @@ public class HealthController : MonoBehaviour
         }
 
         _currentHealth -= damageAmount;
+        OnHealthChanged.Invoke();
 
         if (_currentHealth < 0)
         {
@@ -60,6 +62,7 @@ public class HealthController : MonoBehaviour
         }
 
         _currentHealth += amountToAdd;
+        OnHealthChanged.Invoke();
 
         if (_currentHealth > _maximumHealh)
         {
